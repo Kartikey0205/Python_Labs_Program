16a1f1a2659c85bfdf804d49fb0c4261995c55f3
Kartikey Dubey <66429038+Kartikey0217@users.noreply.github.com>
2020-12-16 22:44:55 +0530

    Updated regular Expression question!!

--- p044.py
+++ p044.py
@@ -39,7 +39,7 @@ while True:
 
 """
 Output:
-Enter the password I_hate_you_from_the_c@re_@f_my_h$art(10th_nov_2020)
+Enter the password I_hate_you_from_the_c@re_@f_my_h$art(19_sep_2019-10th_nov_2020)
 Valid Password
 
 """
